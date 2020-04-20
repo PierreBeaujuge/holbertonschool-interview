9213c166f7ba58a703fb76e3f87e800f1f6d0790
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-04-20 05:41:51 +0000

    input another answer

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,20 +6,26 @@ Lockboxes
 
 def canUnlockAll(boxes):
     """method that determines if all the boxes can be opened"""
-    # key_store = []
-    # for i in range(len(boxes) - 1):
-    #     flag = 0
-    #     for j in range(len(boxes[i])):
-    #         key_store.append(boxes[i][j])
-    #         if boxes[i][j] == i + 1:
-    #             flag = 1
-    #     if i + 1 in key_store:
-    #         continue
-    #     if flag == 0:
-    #         print(key_store)
-    #         print("i: {}".format(i))
-    #         return False
-    # return True
+    key_store = []
+    for i in range(len(boxes) - 1):
+        flag = 0
+        for j in range(len(boxes[i])):
+            key_store.append(boxes[i][j])
+            if boxes[i][j] == i + 1:
+                flag = 1
+        if i + 1 in key_store:
+            continue
+        for k in range(i + 2, len(boxes)):
+            if k in key_store:
+                for l in range(len(boxes[k])):
+                    key_store.append(boxes[k][l])
+                    if boxes[k][l] == i + 1:
+                        flag = 1
+        if flag == 0:
+            # print(key_store)
+            # print("i: {}".format(i))
+            return False
+    return True
 
     # key_store = [key for sublist in boxes for key in sublist]
     # print(key_store)
@@ -36,10 +42,10 @@ def canUnlockAll(boxes):
     #         return False
     # return True
 
-    key_store = [key for sublist in boxes for key in sublist]
-    # print(key_store)
-    for i in range(len(boxes) - 1):
-        if i + 1 in key_store[:i + 1] or i + 1 in key_store[i + 2:]:
-            continue
-        return False
-    return True
+    # key_store = [key for sublist in boxes for key in sublist]
+    # # print(key_store)
+    # for i in range(len(boxes) - 1):
+    #     if i + 1 in key_store[:i + 1] or i + 1 in key_store[i + 2:]:
+    #         continue
+    #     return False
+    # return True
--- 0x00-lockboxes/main_0.py
+++ 0x00-lockboxes/main_0.py
@@ -10,3 +10,35 @@ print(canUnlockAll(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
+
+# expected True
+boxes = []
+print(canUnlockAll(boxes))
+
+# expected True
+boxes = [[]]
+print(canUnlockAll(boxes))
+
+# expected True
+boxes = [[1]]
+print(canUnlockAll(boxes))
+
+# expected True
+boxes = [[2]]
+print(canUnlockAll(boxes))
+
+# expected True
+boxes = [[1], [2]]
+print(canUnlockAll(boxes))
+
+# expected False
+boxes = [[3], [2]]
+print(canUnlockAll(boxes))
+
+# expected True
+boxes = [[1], []]
+print(canUnlockAll(boxes))
+
+# expected False
+boxes = [[], [2]]
+print(canUnlockAll(boxes))
