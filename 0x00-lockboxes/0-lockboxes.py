09d01fb0bc7142454cf1b59e837a0dec12b0da88
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-04-20 05:50:04 +0000

    clean up

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -22,26 +22,11 @@ def canUnlockAll(boxes):
                     if boxes[k][l] == i + 1:
                         flag = 1
         if flag == 0:
-            # print(key_store)
-            # print("i: {}".format(i))
             return False
     return True
 
-    # key_store = [key for sublist in boxes for key in sublist]
-    # print(key_store)
-    # for i in range(len(boxes) - 1):
-    #     flag = 0
-    #     for j in range(len(boxes[i])):
-    #         key_store.append(boxes[i][j])
-    #         if boxes[i][j] == i + 1:
-    #             flag = 1
-    #     if i + 1 in key_store:
-    #         continue
-    #     if flag == 0:
-    #         print("i: {}".format(i))
-    #         return False
-    # return True
-
+    # this code assumes that all the boxes can be opened (not true),
+    # except the one under consideration:
     # key_store = [key for sublist in boxes for key in sublist]
     # # print(key_store)
     # for i in range(len(boxes) - 1):
