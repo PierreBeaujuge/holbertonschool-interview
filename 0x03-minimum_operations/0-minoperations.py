944021b27f2240e94862cecbdfd406f946902177
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-05-13 01:16:48 +0000

    try while loop

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -17,8 +17,11 @@ def minOperations(n):
     if n <= 1:
         return 0
     num_op = []
-    for i in range(2, int(n) + 1):
-        while (n % i) == 0:
+    i = 2
+    while i < n + 1:
+        if (n % i) == 0:
             num_op += [i]
-            n = n // i
+            n = n / i
+        else:
+            i += 1
     return sum(num_op)
