e0756a24caf59c0eaad45f4fced596addbec5efa
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-05-24 18:35:28 +0000

    cleaned up code

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -9,32 +9,54 @@
 int is_palindrome(listint_t **head)
 {
 	int i = 0, j = 0;
-	/* int len = 0, i = 0, j = 0; */
 	listint_t *current = NULL;
 	int array[1000000];
-	/* int *array = NULL; */
 
 	if (!head)
 		return (1);
 	if (!(*head))
 		return (1);
 	current = *head;
-	/* while (current) */
-	/*	len++, current = current->next; */
-	/* current = *head; */
-	/* array = malloc(sizeof(int) * len); */
-	/* if (!array) */
-	/*	return (0); */
 	while (current)
 	{
 		array[i] = current->n;
 		current = current->next;
 		i++;
 	}
-	/* for (j = 0; j < len; j++) */
 	for (j = 0; j < i; j++)
-		/* if (array[j] != array[len - 1 - j]) */
 		if (array[j] != array[i - 1 - j])
 			return (0);
 	return (1);
 }
+
+/**
+ * malloc - this code missed one efficiency check
+ * int is_palindrome(listint_t **head)
+ * {
+ *	int len = 0, i = 0, j = 0;
+ *	listint_t *current = NULL;
+ *	int *array = NULL;
+ *
+ *	if (!head)
+ *		return (1);
+ *	if (!(*head))
+ *		return (1);
+ *	current = *head;
+ *	while (current)
+ *		len++, current = current->next;
+ *	current = *head;
+ *	array = malloc(sizeof(int) * len);
+ *	if (!array)
+ *		return (0);
+ *	while (current)
+ *      {
+ *		array[i] = current->n;
+ *		current = current->next;
+ *		i++;
+ *	}
+ *	for (j = 0; j < len; j++)
+ *		if (array[j] != array[len - 1 - j])
+ *			return (0);
+ *	return (1);
+ * }
+ */
