56da50f5212cf9e151b98ce36cf702cdc4f9fa11
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-06-29 00:39:11 +0000

    input answers

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,100 @@
+#include "slide_line.h"
+
+/**
+ * slide_line - function that slides and merges an array of integers
+ * @line: points to an array of integers
+ * @size: number of elements contained in the array line
+ * @direction: sliding & merging direction, SLIDE_LEFT or SLIDE_RIGHT
+ * Return: 1 upon success, or 0 upon failure
+ */
+
+int slide_line(int *line, size_t size, int direction)
+{
+	if (!line || !size ||
+	    (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
+		return (0);
+	direction == SLIDE_LEFT ?
+		slide_left(line, size)
+		: slide_right(line, size);
+	return (1);
+}
+
+/**
+ * slide_left - function that slides and merges to the left
+ * @line: points to an array of integers
+ * @size: number of elements contained in the array line
+ * Return: void
+ */
+
+void slide_left(int *line, size_t size)
+{
+	size_t i, j, k, l;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		l = i;
+		if (!line[l])
+		{
+			for (k = l + 1; k < size - 1; k++)
+			{
+				if (line[k])
+				{
+					line[l] = line[k];
+					line[k] = 0;
+					l++;
+				}
+			}
+		}
+		if (line[i] && line[i + 1] &&
+			line[i] == line[i + 1])
+		{
+			line[i] = line[i] * 2;
+			for (j = i; j < size - 1; j++)
+			{
+				line[j + 1] = line[j + 2];
+				if (j + 2 == size - 1)
+					line[j + 2] = 0;
+			}
+		}
+	}
+}
+
+/**
+ * slide_right - function that slides and merges to the right
+ * @line: points to an array of integers
+ * @size: number of elements contained in the array line
+ * Return: void
+ */
+
+void slide_right(int *line, size_t size)
+{
+	size_t i, j, k, l;
+
+	for (i = size - 1; i > 0; i--)
+	{
+		l = i;
+		if (!line[l])
+		{
+			for (k = l - 1; k > 0; k--)
+			{
+				if (line[k])
+				{
+					line[l] = line[k];
+					line[k] = 0;
+					l--;
+				}
+			}
+		}
+		if (line[i] && line[i - 1] &&
+		    line[i] == line[i - 1])
+		{
+			line[i] = line[i] * 2;
+			for (j = i; j > 0; j--)
+			{
+				line[j - 1] = line[j - 2];
+				if (j - 2 == 0)
+					line[j - 2] = 0;
+			}
+		}
+	}
+}
---
+++ 0x0A-slide_line/README.md
@@ -0,0 +1,1 @@
+Learn and practice interview prep algorithms
