247c6609f926e9cac926c46a2d177b6a937acca9
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-08-17 05:06:30 +0000

    ran semistandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,24 +4,24 @@ const myArray = process.argv.slice(2);
 const request = require('request');
 
 if (Number.isInteger(parseInt(myArray[0]))) {
-    request('https://swapi.dev/api/films/' + myArray[0],
-	    async (error, response, body) => {
-		if (error) console.error('error:', error);
-		const linksArray = JSON.parse(body).characters;
-		for (let index = 0; index < linksArray.length; index++) {
-		    let name = await getName(linksArray[index]);
-		    console.log(name);
-		}
-	    })
-};
+  request('https://swapi.dev/api/films/' + myArray[0],
+    async (error, response, body) => {
+      if (error) console.error('error:', error);
+      const linksArray = JSON.parse(body).characters;
+      for (let index = 0; index < linksArray.length; index++) {
+        const name = await getName(linksArray[index]);
+        console.log(name);
+      }
+    });
+}
 
 function getName (link) {
-    const name = new Promise(
-	(resolve, reject) => {
-	    request(link, (error, response, body) => {
-		if (error) console.error('error:', error);
-		resolve(JSON.parse(body).name);
-	    })
-	})
-    return name;
-};
+  const name = new Promise(
+    (resolve, reject) => {
+      request(link, (error, response, body) => {
+        if (error) console.error('error:', error);
+        resolve(JSON.parse(body).name);
+      });
+    });
+  return name;
+}
